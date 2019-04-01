c85aa061f35ab5446b690273ebd6c71b236e36c6
Neill Robson <neill@neillrobson.com>
2019-03-31 20:04:57 -0400

    Add shuffle and reset

--- src/app/deck.jsx
+++ src/app/deck.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 
 import './deck.css';
 
+const INITIAL_STATE = {
+    currentCard: null,
+    drawPile: ['red', 'orange', 'yellow', 'green', 'blue', 'purple'],
+    discardPile: []
+}
+
+const clone = x => JSON.parse(JSON.stringify(x));
+
 export default class Deck extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            currentCard: null,
-            drawPile: ['red', 'orange', 'yellow', 'green', 'blue', 'purple'],
-            discardPile: []
-        };
+        this.state = clone(INITIAL_STATE);
         this.handleClick = this.handleClick.bind(this);
+        this.shuffle = this.shuffle.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     handleClick() {
@@ -25,8 +31,23 @@ export default class Deck extends React.Component {
         }));
     }
 
+    shuffle() {
+        let drawPile = this.state.drawPile.slice();
+        for (let i = drawPile.length - 1; i > 0; --i) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let x = drawPile[i];
+            drawPile[i] = drawPile[j];
+            drawPile[j] = x;
+        }
+        this.setState({ drawPile });
+    }
+
+    reset() {
+        this.setState({ ...clone(INITIAL_STATE) });
+    }
+
     render() {
-        let className = 'current-card ' + this.state.currentCard;
+        let cardClassName = 'current-card ' + this.state.currentCard;
         let message;
         if (!this.state.currentCard) {
             if (this.state.drawPile.length === 0) {
@@ -36,8 +57,14 @@ export default class Deck extends React.Component {
             }
         }
         return (
-            <div className={className} onClick={this.handleClick}>
-                {message}
+            <div className="deck">
+                <div className={cardClassName} onClick={this.handleClick}>
+                    {message}
+                </div>
+                <div className="controls">
+                    <button onClick={this.shuffle}>Shuffle</button>
+                    <button onClick={this.reset}>Reset</button>
+                </div>
             </div>
         );
     }
