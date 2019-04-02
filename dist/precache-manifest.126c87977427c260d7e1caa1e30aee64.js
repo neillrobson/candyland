0b6beb4256911670515142109f3036047516d60d
Neill Robson <neill@neillrobson.com>
2019-04-02 19:31:38 -0400

    Remove dist files

--- dist/precache-manifest.126c87977427c260d7e1caa1e30aee64.js
+++
@@ -1,9 +0,0 @@
-self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "4a853ffbd361da5fb63d",
-    "url": "main.c7310a4d3e45ac534aaa.css"
-  },
-  {
-    "url": "main.4a853ffbd361da5fb63d.js"
-  }
-]);
